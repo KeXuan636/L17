2c2f497231ed4802cde06813b786ecbd6bbf96c3
KeXuan636 <kexuanlee0709@gmail.com>
2026-01-14 14:32:11 +0800

    hi

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { FlatList, StatusBar, Text, TextInput, View} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const App = () => {
+    const [myData, setMyData] = useState([]);
+
+    const renderItem = ({item, index}) => {
+        return (
+            <View>
+                <Text style={{borderWidth:1}}>{item.card_name}</Text>
+            </View>
+        );
+    };
+
+    return (
+        <View>
+            <StatusBar/>
+            <Text>Search:</Text>
+            <TextInput style={{borderWidth:1}}/>
+            <FlatList data={myData} renderItem={renderItem} />
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
